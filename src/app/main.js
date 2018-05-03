6da62c005bf1dc210040aa2eb740fb089c8f0bed
Ale Lmn <genrih.md@gmail.com>
2018-05-03 17:13:51 +0400

    shedule commit: work with header menu

--- src/app/main.js
+++ src/app/main.js
@@ -1,6 +1,6 @@
 import './plugins/vanilla.js';
 import $ from '../../node_modules/jquery/dist/jquery.js';
-import '../../node_modules/bootstrap/dist/js/bootstrap.js';
+import '../../node_modules/bootstrap/dist/js/bootstrap.bundle.js';
 
 document.addEventListener('DOMContentLoaded', letsGetItStarted);
 function letsGetItStarted(){
