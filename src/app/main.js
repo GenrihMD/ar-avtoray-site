f40ecbf4278d1321792e17c4f00f031f8978f5da
Ale Lmn <genrih.md@gmail.com>
2018-05-04 03:32:57 +0400

    do done dropdown menu + do burger menu

--- src/app/main.js
+++ src/app/main.js
@@ -2,9 +2,12 @@ import './plugins/vanilla.js';
 import $ from '../../node_modules/jquery/dist/jquery.js';
 import '../../node_modules/bootstrap/dist/js/bootstrap.bundle.js';
 
-document.addEventListener('DOMContentLoaded', main);
-function main(){
+const toggleClassSet = function () { this.classList.toggle('set'); };
+const removeClassSet = function () { this.classList.remove('set'); };
 
+document.addEventListener('DOMContentLoaded', main);
+
+function main() {
   document
     .querySelectorAll('.by-click-changable')
     .addEventListener('click', toggleClassSet);
@@ -12,13 +15,4 @@ function main(){
   document
     .querySelectorAll('.by-mouseleave-resetable')
     .addEventListener('mouseleave', removeClassSet);
-
 }
-
-const toggleClassSet = function () {
-  this.classList.toggle('set');
-};
-
-const removeClassSet = function () {
-  this.classList.remove('set');
-};
