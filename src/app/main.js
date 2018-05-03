5df7712d85202bd93e3683c3c3a4779d198dbe0c
Ale Lmn <genrih.md@gmail.com>
2018-05-03 20:09:02 +0400

    do made dropedown menu

--- src/app/main.js
+++ src/app/main.js
@@ -2,13 +2,23 @@ import './plugins/vanilla.js';
 import $ from '../../node_modules/jquery/dist/jquery.js';
 import '../../node_modules/bootstrap/dist/js/bootstrap.bundle.js';
 
-document.addEventListener('DOMContentLoaded', letsGetItStarted);
-function letsGetItStarted(){
+document.addEventListener('DOMContentLoaded', main);
+function main(){
 
   document
     .querySelectorAll('.by-click-changable')
-    .addEventListener('click', function () {
-      this.classList.toggle('set');
-    });
+    .addEventListener('click', toggleClassSet);
+
+  document
+    .querySelectorAll('.by-mouseleave-resetable')
+    .addEventListener('mouseleave', removeClassSet);
 
 }
+
+const toggleClassSet = function () {
+  this.classList.toggle('set');
+};
+
+const removeClassSet = function () {
+  this.classList.remove('set');
+};
