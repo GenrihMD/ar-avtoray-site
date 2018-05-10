bb2c697005050ec4304e5e0a66a9b1f7f5fab42c
Ale Lmn <genrih.md@gmail.com>
2018-05-11 01:40:17 +0400

    start make bitrix template

---
+++ site_root/local/templates/aavtoray/description.php
@@ -0,0 +1,9 @@
+<?
+if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
+	die();
+
+$arTemplate = Array(
+	'NAME' => 'Авторай.al',
+	'DESCRIPTION' => 'Avtoray template by Ale Lmn'
+);
+?>
