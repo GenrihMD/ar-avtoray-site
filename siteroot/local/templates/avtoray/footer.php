172c53b9d67b300272784e80de1f2482cb41dbb5
Ale Lmn <genrih.md@gmail.com>
2018-05-25 03:34:54 +0300

    add copyright in footer

--- siteroot/local/templates/avtoray/footer.php
+++ siteroot/local/templates/avtoray/footer.php
@@ -1,8 +1,13 @@
 <?
-if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
-	die();
+if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
+    die();
 ?>
 
+        <footer>
+            <div class="copyright">
+                <div class="container">Авторай © 2018. Все права защищены.</div>
+            </div>
+        </footer>
         <script src="/assets/app/main.js"></script>
-	</body>
+    </body>
 </html>
